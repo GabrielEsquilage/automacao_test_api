8882afd9ec22a652bb269415a76bb1aa817cf64b
Gabriel Esquilage <roccaesquil19910@gmail.com>
2026-04-02 15:39:50 -0300

    criação teste de integração com moodle / acesso a disciplina

--- src/test/java/api/MetodosPublicos.java
+++ src/test/java/api/MetodosPublicos.java
@@ -5,36 +5,57 @@ import io.restassured.response.Response;
 
 public class MetodosPublicos {
 
-    
-    public static Response realizarLoginDev(String login, String password) {
+    public static Response realizarLoginPortalProd(String login, String senha) {
+        String corpoJson = "{\"login\": \"" + login + "\", \"senha\": \"" + senha + "\"}";
+        
         return given()
-            .formParam("login", login)
-            .formParam("password", password)
+            .contentType("application/json")
+            .body(corpoJson)
         .when()
-            .post("https://erp-api-stage.inovacarreira.com.br/api/v1/app/auth/login");
+            .post("https://erp-api-prod-964330493122.southamerica-east1.run.app/api-external/v1/portal/auth/login")
+            .then()
+            .extract().response();
     }
 
-    
-    public static Response realizarRefreshTokenDev(String refreshToken) {
+    public static Response buscarMatriculaId(String accessToken) {
         return given()
-            .header("Refresh-Token", "Bearer " + refreshToken)
+            .header("Authorization", "Bearer " + accessToken)
+            .contentType("application/json")
         .when()
-            .post("https://erp-api-stage.inovacarreira.com.br/api/v1/app/auth/token/refresh");
+            .get("https://erp-api-prod-964330493122.southamerica-east1.run.app/api-external/v1/portal/matricula/matricula-disciplina")
+            .then()
+            .extract().response();
     }
-    
-    public static Response realizarLoginPortalProd(String login, String senha) {
+
+    public static Response buscarDisciplinasAtuaisPortalProd(String accessToken, String matriculaId) {
         return given()
-            .formParam("login", login)
-            .formParam("senha", senha)
+            .header("Authorization", "Bearer " + accessToken)
+            .header("matriculaId", matriculaId)
+            .contentType("application/json")
         .when()
-            .post("https://erp-api-prod-964330493122.southamerica-east1.run.app/api-external/v1/portal/auth/login");
+            .get("https://erp-api-prod-964330493122.southamerica-east1.run.app/api-external/v1/portal/moodle/disciplinas-current")
+            .then()
+            .extract().response();
     }
-    
-    public static Response realizarLoginPortalDev(String login, String senha) {
+
+    public static Response buscarDetalhesMoodle(String accessToken, String courseMoodleId, String ra, String matriculaId, String educationLevel) {
         return given()
-            .formParam("login", login)
-            .formParam("senha", senha)
+            .header("Authorization", "Bearer " + accessToken)
+            .header("ra", ra)
+            .header("matriculaId", matriculaId)
+            .header("educationLevel", educationLevel)
+            .contentType("application/json")
         .when()
-            .post("https://erp-api-dev-922117522963.us-central1.run.app/api-external/v1/portal/auth/login");
+            .get("https://erp-api-prod-964330493122.southamerica-east1.run.app/api-external/v1/portal/moodle/disciplina-link/" + courseMoodleId)
+            .then()
+            .extract().response();
+    }
+
+    public static Response acessarLinkMoodle(String url) {
+        return given()
+        .when()
+            .post(url)
+            .then()
+            .extract().response();
     }
 }
