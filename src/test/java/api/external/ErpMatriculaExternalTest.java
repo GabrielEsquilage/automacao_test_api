316bdb422d97472c7beda96f47be2c49c4df8289
Gabriel Esquilage <roccaesquil19910@gmail.com>
2026-06-26 13:24:43 -0300

    test: adicionar testes de contrato para api externa de matriculas

--- src/test/java/api/external/ErpMatriculaExternalTest.java
+++ src/test/java/api/external/ErpMatriculaExternalTest.java
@@ -1,81 +1,74 @@
 package api.external;
 
-import static io.restassured.RestAssured.given;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import api.support.ApiConfig;
-import api.support.AuthManager;
+import api.support.ApiReportExtension;
+import api.support.ErpApiClient;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
-@DisplayName("Testes de Contrato - API Externa (Matricula)")
-public class ErpMatriculaExternalTest extends ApiConfig {
+@Tag("api")
+@Tag("erp")
+@Tag("contrato")
+@DisplayName("ERP - Testes da API Externa (Matricula)")
+@ExtendWith(ApiReportExtension.class)
+public class ErpMatriculaExternalTest {
 
     @Test
-    @Tag("contrato")
     @DisplayName("Valida contrato da lista de gêneros na API Externa")
     void testGendersList() {
-        given()
-            .header("Authorization", "Bearer " + AuthManager.getToken())
-        .when()
-            .get("/api-external/v1/matricula/genders")
-        .then()
-            .statusCode(200)
-            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayGeneroRecordCleanDTO.json"));
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/matricula/genders")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayGeneroRecordCleanDTO.json"));
     }
 
     @Test
-    @Tag("contrato")
     @DisplayName("Valida contrato da lista de raças na API Externa")
     void testRacesList() {
-        given()
-            .header("Authorization", "Bearer " + AuthManager.getToken())
-        .when()
-            .get("/api-external/v1/matricula/races")
-        .then()
-            .statusCode(200)
-            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayRacaRecordCleanDTO.json"));
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/matricula/races")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayRacaRecordCleanDTO.json"));
     }
 
     @Test
-    @Tag("contrato")
     @DisplayName("Valida contrato da lista de origens na API Externa")
     void testOriginList() {
-        given()
-            .header("Authorization", "Bearer " + AuthManager.getToken())
-        .when()
-            .get("/api-external/v1/matricula/origin")
-        .then()
-            .statusCode(200)
-            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayTipoOrigemMatriculaDisciplinaRecordCleanDTO.json"));
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/matricula/origin")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayTipoOrigemMatriculaDisciplinaRecordCleanDTO.json"));
     }
 
     @Test
-    @Tag("contrato")
     @DisplayName("Valida contrato da lista de tipos de pagamento na API Externa")
     void testPaymentTypeList() {
-        given()
-            .header("Authorization", "Bearer " + AuthManager.getToken())
-        .when()
-            .get("/api-external/v1/matricula/payment-type")
-        .then()
-            .statusCode(200)
-            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayFormaPagamentoRecordCleanDTO.json"));
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/matricula/payment-type")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayFormaPagamentoRecordCleanDTO.json"));
     }
 
     @Test
-    @Tag("contrato")
     @DisplayName("Valida contrato da lista de estados na API Externa")
     void testStatesList() {
-        given()
-            .header("Authorization", "Bearer " + AuthManager.getToken())
-        .when()
-            .get("/api-external/v1/matricula/states")
-        .then()
-            .statusCode(200)
-            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayUfRecordCleanDTO.json"));
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/matricula/states")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayUfRecordCleanDTO.json"));
     }
 
 }
