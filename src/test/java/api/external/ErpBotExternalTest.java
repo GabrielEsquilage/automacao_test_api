b5b29a6e249383f8e4f9ca0743c63f483799a970
Gabriel Esquilage <roccaesquil19910@gmail.com>
2026-06-26 13:42:36 -0300

    test: adicionar testes de contrato e schemas para api-external bot e tratar nulls

---
+++ src/test/java/api/external/ErpBotExternalTest.java
@@ -0,0 +1,54 @@
+package api.external;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import api.support.ApiReportExtension;
+import api.support.ErpApiClient;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+@Tag("api")
+@Tag("erp")
+@Tag("contrato")
+@DisplayName("ERP - Testes da API Externa (Bot)")
+@ExtendWith(ApiReportExtension.class)
+public class ErpBotExternalTest {
+
+    @Test
+    @DisplayName("Valida contrato das matrículas acessíveis ao Bot")
+    void testBotMatriculas() {
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/bot/matriculas")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayMatriculaProjectionForBotMatriculaDisciplina.json"));
+    }
+
+    @Test
+    @DisplayName("Valida contrato dos cursos acessíveis ao Bot")
+    void testBotCursos() {
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/bot/cursos")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/PageCursoWithWaeProjectionDTO.json"));
+    }
+
+
+
+    @Test
+    @DisplayName("Valida contrato dos assuntos de mensageria do Bot")
+    void testBotAssuntosMensageria() {
+        ErpApiClient.request()
+            .when()
+                .get("/api-external/v1/bot/assuntos-mensageria")
+            .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/ArrayResponseForBotDTO.json"));
+    }
+
+}
